3f4f85e335aa09a092dbf12ac76467912b1a16d0
Leland Batey <lelandbatey@lelandbatey.com>
2017-10-24 11:07:54 -0700

    Port graphapi and Azure-ad login main app
    This commit adds all the data and functionality implemented in the Azure
    sample `active-directory-dotnet-graphapi-web` found at this url:
    https://github.com/Azure-Samples/active-directory-dotnet-graphapi-web

---
+++ OneRing/OneRing/Utils/GraphConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+
+namespace OneRing.Utils
+{
+	internal static class GraphConfiguration
+	{
+		private const string tenantIdClaimType = "http://schemas.microsoft.com/identity/claims/tenantid";
+		private static string graphResourceId = ConfigurationManager.AppSettings["ida:GraphUrl"];
+		private static string graphApiVersion = ConfigurationManager.AppSettings["ida:GraphApiVersion"];
+
+		internal static string GraphApiVersion
+		{
+			get { return graphApiVersion; }
+			set { graphApiVersion = value; }
+		}
+
+		internal static string GraphResourceId
+		{
+			get { return graphResourceId; }
+			set { graphResourceId = value; }
+		}
+
+		internal static string TenantIdClaimType
+		{
+			get { return tenantIdClaimType; }
+		}
+	}
+}
