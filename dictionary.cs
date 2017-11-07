09661eb3dcd97654c2ddaa916a3301a03b5bd065
scott.kovacs <scott.kovacs@wsu.edu>
2017-11-06 20:32:37 -0800

    first go at a dictionary...

---
+++ dictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    class Example
+    {
+        List<string> values = new List<string>
+    {
+        {"www.example.com"},
+        {"www.google.com" }
+    };
+
+        Dictionary<string, List<string>> _dict = new Dictionary<string, List<string>>();
+
+        public List<string> GetURL(string portlet_id)
+        {
+            // Try to get the result in the Dictionary
+            List<string> urlList;
+            if (_dict.TryGetValue(portlet_id, out urlList))
+            {
+                return urlList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void AddDataSource(string portlet_id, List<string> values, string newURL)
+        {
+            if (!(values.Contains(newURL)))
+            {
+                values.Add(newURL);
+            }
+
+            _dict.Add(portlet_id, values);
+        }
+    }
+}
